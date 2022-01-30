c1b3e5b8d0a49ea4e1468d89f6b1293c30c9ec58
IrineDevanbu <steffnosdev@gmail.com>
2022-01-30 17:34:11 +0530

    steff newcode

--- src/test/java/org/classd/A.java
+++ src/test/java/org/classd/A.java
@@ -6,5 +6,8 @@ public class A {
 public static void main(String[] args) {
 	
 	System.out.println("test1");
+	System.out.println("test7");
+	System.out.println("test8");
+	System.out.println("test9");
 }
 }
