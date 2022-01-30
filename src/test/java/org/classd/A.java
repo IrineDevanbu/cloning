9ef6ffd2b9dc4cbac33a58907f0ef222039eee9a
IrineDevanbu <steffnosdev@gmail.com>
2022-01-30 17:25:38 +0530

    irine updated

--- src/test/java/org/classd/A.java
+++ src/test/java/org/classd/A.java
@@ -6,5 +6,8 @@ public class A {
 public static void main(String[] args) {
 	
 	System.out.println("test1");
+	System.out.println("test2");
+	System.out.println("test3");
+	System.out.println("test4");
 }
 }
